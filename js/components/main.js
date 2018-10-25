f078bb3742d85c27acc712d2c2fef3e58e40df87
nasitra <nasitra@users.noreply.github.com>
2018-10-25 22:24:55 +0900

    Make 'content' component

--- js/components/main.js
+++ js/components/main.js
@@ -2,8 +2,11 @@
   'use strict';
 
   var jCore = require('jcore');
+  var Content = app.Content || require('./content.js');
 
-  var Main = jCore.Component.inherits();
+  var Main = jCore.Component.inherits(function() {
+    this.content = new Content({ element: this.findElement('.content') });
+  });
 
   if (typeof module !== 'undefined' && module.exports) {
     module.exports = Main;
