f51d64c24ef378979987248cfeb46c2db8922ea9
nasitra <nasitra@users.noreply.github.com>
2018-10-28 19:49:01 +0900

    Make utility method to set CSS styles of an element

--- js/dom.js
+++ js/dom.js
@@ -7,6 +7,13 @@
     return document.body;
   };
 
+  dom.css = function(el, props) {
+    var style = el.style;
+    Object.keys(props).forEach(function(key) {
+      style[key] = props[key];
+    });
+  };
+
   if (typeof module !== 'undefined' && module.exports) {
     module.exports = dom;
   } else {
