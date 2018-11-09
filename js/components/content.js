5464e7a4d71847fff578451beb5bb970e3101c2a
nasitra <nasitra@users.noreply.github.com>
2018-11-10 08:41:45 +0900

    Add 'top' property of 'content' component

--- js/components/content.js
+++ js/components/content.js
@@ -2,10 +2,18 @@
   'use strict';
 
   var jCore = require('jcore');
+  var dom = app.dom || require('../dom.js');
 
   var Content = jCore.Component.inherits(function() {
+    this.top = this.prop(0);
     this.pages = [];
   });
+
+  Content.prototype.onredraw = function() {
+    this.redrawBy('top', function(top) {
+      dom.translateY(this.element(), top);
+    });
+  };
 
   if (typeof module !== 'undefined' && module.exports) {
     module.exports = Content;
