474e1a388820873595070032e4d5c27750cc5200
nasitra <nasitra@users.noreply.github.com>
2018-11-12 18:25:44 +0900

    Make method to set height of 'main' component

--- js/components/main.js
+++ js/components/main.js
@@ -2,11 +2,19 @@
   'use strict';
 
   var jCore = require('jcore');
+  var dom = app.dom || require('../dom.js');
   var Content = app.Content || require('./content.js');
 
   var Main = jCore.Component.inherits(function() {
+    this.height = this.prop(0);
     this.content = new Content({ element: this.findElement('.content') });
   });
+
+  Main.prototype.onredraw = function() {
+    this.redrawBy('height', function(height) {
+      dom.css(this.element(), { height: height + 'px' });
+    });
+  };
 
   if (typeof module !== 'undefined' && module.exports) {
     module.exports = Main;
