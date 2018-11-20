231f6e81f658ba50fba1cccdc38af235ab23ae1e
nasitra <nasitra@users.noreply.github.com>
2018-11-20 21:03:36 +0900

    Add 'visible' property of 'content' component

--- js/components/content.js
+++ js/components/content.js
@@ -6,11 +6,16 @@
 
   var Content = jCore.Component.inherits(function() {
     this.top = this.prop(0);
+    this.visible = this.prop(false);
   });
 
   Content.prototype.onredraw = function() {
     this.redrawBy('top', function(top) {
       dom.translateY(this.element(), top);
+    });
+
+    this.redrawBy('visible', function(visible) {
+      dom.toggleClass(this.element(), 'hide', !visible);
     });
   };
 
