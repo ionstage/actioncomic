ed58e696117ad83e70c83ddcd6f9f4d76668cc5a
nasitra <nasitra@users.noreply.github.com>
2018-11-21 13:25:55 +0900

    Remove 'top' property of 'content' component

--- js/components/content.js
+++ js/components/content.js
@@ -5,15 +5,10 @@
   var dom = app.dom || require('../dom.js');
 
   var Content = jCore.Component.inherits(function() {
-    this.top = this.prop(0);
     this.visible = this.prop(false);
   });
 
   Content.prototype.onredraw = function() {
-    this.redrawBy('top', function(top) {
-      dom.translateY(this.element(), top);
-    });
-
     this.redrawBy('visible', function(visible) {
       dom.toggleClass(this.element(), 'hide', !visible);
     });
